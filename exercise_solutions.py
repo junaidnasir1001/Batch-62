0dc65c480ab7659feef2f53709cd227ae0f46536
junaidnasir1001 <45687809+junaidnasir1001@users.noreply.github.com>
2024-08-10 13:08:35 +0500

    first commit

---
+++ exercise_solutions.py
@@ -0,0 +1,80 @@
+message = "Hello, world!"
+print(message)
+
+message = "Hello, world!"
+print(message)
+message = "Goodbye, world!"
+print(message)
+
+name = "Junaid Nasir"
+print(f"Hello {name}, would you like to learn some Python today?")
+
+name = "Junaid Nasir"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+quote = "A person who never made a mistake never tried anything new."
+author = "Albert Einstein"
+print(f'{author} once said, “{quote}”')
+
+famous_person = "Albert Einstein"
+message = f'{famous_person} once said, “A person who never made a mistake never tried anything new.”'
+print(message)
+
+name = "\t\n Junaid Nasir \n\t"
+print(f"Original: '{name}'")
+print(f"Lstrip: '{name.lstrip()}'")
+print(f"Rstrip: '{name.rstrip()}'")
+print(f"Strip: '{name.strip()}'")
+
+x = 5
+y = 10
+z = 15
+print(x + y + z)
+
+a = 3
+b = 8
+print(f"Before swap: a = {a}, b = {b}")
+a, b = b, a
+print(f"After swap: a = {a}, b = {b}")
+
+favorite_color = "Blue"
+print(favorite_color)
+color = favorite_color
+print(color)
+
+pet_name = "Buddy"
+print(pet_name)
+pet_name = "Max"
+print(pet_name)
+
+# I already observe the following code error which is NameError: name 'sunshine' is not defined. Did you mean: 'sunsine'?
+sunshine = "Sunshine"
+print(sunshine)
+
+score = 100
+print(score)
+score = 200
+print(score)
+
+city = "Lahore"
+print(city)
+
+text = "python programming"
+print(text.title())
+
+mixed_case_string = "PyThOn PRogrAMmiNg"
+print(mixed_case_string.lower())
+
+mixed_case_string = "PyThOn PRogrAMmiNg"
+print(mixed_case_string.upper())
+
+temperature = 25
+print(f"The current temperature is {temperature} degrees.")
+
+poem = """Two roads diverged in a yellow wood,
+And sorry I could not travel both
+And be one traveler, long I stood
+And looked down one as far as I could"""
+print(poem)
